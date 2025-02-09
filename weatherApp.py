624d2f70b9e3cafbb980ff84ce59c1190f49cb17
Mark Peters <mapeters101@earthlink.net>
2025-02-09 17:28:45 -0500

    Work on lesson004 to 2:00 for weatherApp.  Add Icon image.

--- weatherApp.py
+++ weatherApp.py
@@ -14,6 +14,10 @@ root.geometry("890x470+300+300")
 root.configure(bg="#57adff")
 root.resizable(False,False)
 
+#Icon
+image_icon = PhotoImage(file="images/logo.png")
+root.iconphoto(False,image_icon)
+
 mainloop()
 
 
