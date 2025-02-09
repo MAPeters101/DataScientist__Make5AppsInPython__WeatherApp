ca498dea0d5a5acb770220a12e83f326a944e65c
Mark Peters <mapeters101@earthlink.net>
2025-02-09 17:20:46 -0500

    Work on lesson003 to 3:10 for weatherApp.  First window display.

---
+++ weatherApp.py
@@ -0,0 +1,18 @@
+
+from tkinter import *
+import tkinter as tk
+from geopy.geocoders import Nominatim
+from timezonefinder import TimezoneFinder
+from datetime import *
+import requests
+import pytz
+from PIL import Image,ImageTk
+
+root = Tk()
+
+
+mainloop()
+
+
+
+
