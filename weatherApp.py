df6b495f78cb251cb5e7e38f8aae006ddb5108e5
Mark Peters <mapeters101@earthlink.net>
2025-02-10 00:23:51 -0500

    Complete work on lesson004 from 5:50 for weatherApp.  Add additional Labels.

--- weatherApp.py
+++ weatherApp.py
@@ -25,6 +25,18 @@ Label(root,image=Round_box,bg='#57adff').place(x=30,y=110)
 label1 = Label(root,text="Temperature",font=("Helvetica",11),fg="white",bg='#203243')
 label1.place(x=50,y=120)
 
+label2 = Label(root,text="Humidity",font=("Helvetica",11),fg="white",bg='#203243')
+label2.place(x=50,y=140)
+
+label3 = Label(root,text="Pressuree",font=("Helvetica",11),fg="white",bg='#203243')
+label3.place(x=50,y=160)
+
+label4 = Label(root,text="Wind Speed",font=("Helvetica",11),fg="white",bg='#203243')
+label4.place(x=50,y=180)
+
+label5 = Label(root,text="Description",font=("Helvetica",11),fg="white",bg='#203243')
+label5.place(x=50,y=200)
+
 mainloop()
 
 
